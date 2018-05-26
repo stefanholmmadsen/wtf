095bd017427c0ad4d6f08c7a2fdf15944468bfa9
StefanHolmMadsen <stefanholmmadsen@gmail.com>
2018-05-26 14:22:04 +0200

    commit flag

--- buildtest.ps1
+++ buildtest.ps1
@@ -1,10 +1,9 @@
-Write-output $env:TRAVIS_BRANCH
-Write-output $env:TRAVIS_PULL_REQUEST
-Write-output $env:TRAVIS_EVENT_TYPE
-Write-output $env:TRAVIS_COMMIT
+Write-output "branch: "$env:TRAVIS_BRANCH
+Write-output "pull request: "$env:TRAVIS_PULL_REQUEST
+Write-output "event type: "$env:TRAVIS_EVENT_TYPE
+Write-output "commit: " $env:TRAVIS_COMMIT
 
 Write-output "testing current event"
-
 
 #merge events 
 git rev-list --all --min-parents=2
