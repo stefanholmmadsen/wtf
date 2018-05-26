dfc81fdfc61e644434d81ed27882a6f82a4b26b1
StefanHolmMadsen <stefanholmmadsen@gmail.com>
2018-05-26 14:13:21 +0200

    new stuff

--- buildtest.ps1
+++ buildtest.ps1
@@ -1,15 +1,11 @@
- Volume in drive C has no label.
- Volume Serial Number is A25E-4918
+Write-output $TRAVIS_BRANCH
+Write-output $TRAVIS_PULL_REQUEST
+Write-output $TRAVIS_EVENT_TYPE
 
- Directory of c:\Dev\git\wtf
+Write-output "testing current event"
 
-26-05-2018  13:59    <DIR>          .
-26-05-2018  13:59    <DIR>          ..
-26-05-2018  13:50               485 .travis.yml
-26-05-2018  13:59                 0 buildtest.ps1
-26-05-2018  13:50               518 dir.txt
-18-05-2018  21:50                17 dockerfile
-26-05-2018  13:46               473 file.txt
-18-05-2018  21:03                11 README.md
-               6 File(s)          1.504 bytes
-               2 Dir(s)  142.357.245.952 bytes free
+
+#merge events 
+git rev-list --all --min-parents=2
+git log -1 --pretty=oneline
+
